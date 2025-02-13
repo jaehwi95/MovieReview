3047c202a4769dc02bb7f1d22ec65f17556d1eb0
Jaehwi Kim <jaehwi95@gmail.com>
2025-02-13 19:21:57 +0900

    init project
    - init project

---
+++ MovieReview/MovieReview/MovieReviewApp.swift
@@ -0,0 +1,17 @@
+//
+//  MovieReviewApp.swift
+//  MovieReview
+//
+//  Created by Jaehwi Kim on 2/13/25.
+//
+
+import SwiftUI
+
+@main
+struct MovieReviewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
